d98962ba619c4c4ba6136a3002b3873f335faa7c
ExBrain <zecarlosinf@gmail.com>
2023-12-07 15:31:44 +0000

    Added bitsdojo and custom windows bar

--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:dartx/dartx.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -52,15 +53,25 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
+    const windowsBar = _WindowBar();
     return Material(
       color: context.themeColors.mainColor0,
       child: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: _menuWidth),
+          Positioned.fill(
+            left: _menuWidth,
+            top: windowsBar.preferredSize.height,
             child: _pageWidget(),
           ),
-          _buttonsWidget(),
+          Positioned.fill(
+            top: windowsBar.preferredSize.height,
+            right: null,
+            child: _buttonsWidget(),
+          ),
+          const Positioned.fill(
+            bottom: null,
+            child: windowsBar,
+          ),
           Positioned(
             right: 0,
             bottom: 0,
@@ -264,4 +275,68 @@ class _Menu {
             return TrackerRouter.onGenerate(settings);
           },
         );
+}
+
+class _WindowBar extends StatelessWidget implements PreferredSizeWidget {
+  const _WindowBar();
+
+  @override
+  Widget build(BuildContext context) {
+    final mainColor = context.themeColors.mainColor1;
+    final whiteColor = context.themeColors.almostWhite;
+    final mouseDown = Color.lerp(mainColor, whiteColor, 0.1);
+    final mouseOver = Color.lerp(mainColor, whiteColor, 0.2);
+    final colors = WindowButtonColors(
+      mouseDown: mouseDown,
+      mouseOver: mouseOver,
+      iconNormal: whiteColor,
+    );
+
+    return StatefulBuilder(
+      builder: (context, setState) {
+        return Container(
+          height: 32,
+          color: context.themeColors.mainColor1,
+          child: Row(
+            children: [
+              Expanded(
+                child: MoveWindow(
+                  onDoubleTap: () => setState(appWindow.maximizeOrRestore),
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(left: kGridSeparator * 2),
+                    child: Text(
+                      'Genshin Tracker',
+                      style: context.themeStyles.label12b,
+                      strutStyle: context.themeStyles.label12b.toStrut(),
+                    ),
+                  ),
+                ),
+              ),
+              MinimizeWindowButton(colors: colors),
+              appWindow.isMaximized
+                  ? RestoreWindowButton(
+                      colors: colors,
+                      onPressed: () => setState(appWindow.maximizeOrRestore),
+                    )
+                  : MaximizeWindowButton(
+                      colors: colors,
+                      onPressed: () => setState(appWindow.maximizeOrRestore),
+                    ),
+              CloseWindowButton(
+                colors: WindowButtonColors(
+                  mouseOver: const Color(0xFFD32F2F),
+                  mouseDown: const Color(0xFFB71C1C),
+                  iconNormal: whiteColor,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(32);
 }
